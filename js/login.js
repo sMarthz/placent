c1a44312ff96f7b4e8329c725ec2bd978bd26232
sMarthz <72910166+sMarthz@users.noreply.github.com>
2022-04-23 18:10:07 +0200

    Login

---
+++ js/login.js
@@ -0,0 +1,11 @@
+function Login(){ 
+    const email = document.getElementById("email");
+    const password = document.getElementById("password");
+
+    var userValue=email.value; 
+    var passwordValue=password.value; 
+
+   
+    window.location="game.html"; 
+
+} 

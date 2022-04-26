827f202aa49499ac69da721bd33d03d0f514aa36
sMarthz <72910166+sMarthz@users.noreply.github.com>
2022-04-26 19:55:30 +0200

    Temporizador limitador de pixeles

--- js/canvas.js
+++ js/canvas.js
@@ -4,6 +4,11 @@ let size = sizeEl.value
 const color = document.querySelector('.color')
 const resetBtn = document.querySelector('.btn')
 
+let contador = 0;
+let temporizador = 10;
+let puedeDibujar = true;
+let intervalo
+
 let draw = false
 
 function populate(size) {
@@ -12,35 +17,49 @@ function populate(size) {
     const div = document.createElement('div')
     div.classList.add('pixel')
     div.setAttribute('pixel-id', i)
-    div.addEventListener('mouseover', function(){
+    /* div.addEventListener('mouseover', function(){
         if(!draw) return
         div.style.backgroundColor = color.value
-    })
+    }) */
     div.addEventListener('mousedown', function(){
-        div.style.backgroundColor = color.value
+        if (puedeDibujar) {
+            div.style.backgroundColor = color.value;
+            puedeDibujar = false;
+            intervalo = setInterval(contar, 1000);
+
+            const idPixel = div.getAttribute("pixel-id");
+            const colorPixel = color.value;
+            console.log("idPixel",idPixel)
+            console.log("colorPixel",colorPixel)
+            
+        }
     })
 
     container.appendChild(div)
   }
 }
 
-window.addEventListener("mousedown", function(){
+/* window.addEventListener("mousedown", function(){
     draw = true
-})
+}) */
 window.addEventListener("mouseup", function(){
     draw = false
 })
 
-function reset(){
-    container.innerHTML = ''
-    populate(size)
-}
-
-resetBtn.addEventListener('click', reset)
-
 sizeEl.addEventListener('keyup', function(){
     size = sizeEl.value
-    reset()
 })
+
+function contar() {
+    contador++;
+    if (temporizador <= contador) {
+        clearInterval(intervalo);
+        contador = 0;
+        puedeDibujar = true;
+        alert("Ya puedes volver a dibujar");
+    }
+}
+
+
 
 populate(size)
